111f24e661674e305cde18b3c01b123170c432fd
Sergei Gerasenko <sgerasenko@conversantmedia.com>
2021-12-06 19:01:40 -0600

    Rename pp_requests

--- examples/send_via_requests.py
+++ examples/send_via_requests.py
@@ -2,15 +2,15 @@
 
 import requests
 import pyproxy
-from pyproxy.pp_requests import ProxyClient
+from pyproxy.adapter import ProxyClient
 
 
 def send_via_requests(proto):
     server_host = '9.9.9.9'
     server_port = 9000
 
-    session = requests.session()
-    session = ProxyClient(session, proto, src_addr=(server_host, server_port))
+    session = ProxyClient(requests.session(),
+                          proto, src_addr=(server_host, server_port))
 
     resp = session.get('http://127.0.0.1:2081')
     print(resp.content)
