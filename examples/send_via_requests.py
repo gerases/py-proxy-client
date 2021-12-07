f470ffd2fc8c65178535447625daa05603c37c04
Sergei Gerasenko <sgerasenko@conversantmedia.com>
2021-12-06 19:29:16 -0600

    Add comments to an example

--- examples/send_via_requests.py
+++ examples/send_via_requests.py
@@ -5,13 +5,16 @@ from pyproxy.adapter import ProxyClient
 from pyproxy.const import V1, V2
 
 
-def send_via_requests(proto):
+def send_via_requests(pp_proto):
     server_host = '9.9.9.9'
     server_port = 9000
 
+    # ask to wrap a requests session into our adapter
     session = ProxyClient(requests.session(),
-                          proto, src_addr=(server_host, server_port))
+                          pp_proto,
+                          src_addr=(server_host, server_port))
 
+    # interact with the session as usual
     resp = session.get('http://127.0.0.1:2081')
     print(resp.content)
 
